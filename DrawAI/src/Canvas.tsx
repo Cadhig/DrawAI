a22a640717081ad2ae4c7c08610a1507ee620c0e
Cadence K <cadencehiggins@gmail.com>
2024-06-25 21:55:11 -0500

    grid now takes input when users click on pixel

--- DrawAI/src/Canvas.tsx
+++ DrawAI/src/Canvas.tsx
@@ -1,26 +1,39 @@
-import { grid } from "./assets/grid"
+import React from 'react'
+
 const gridSize = 28
 
 export default function Canvas() {
-    let canvasGridComponents = []
-    for (let i = 0; i < gridSize ** 2; i++) {
-        canvasGridComponents.push(<CanvasGrid test='hey' content={<canvas className="w-4 h-4"></canvas>} />)
-    }
+    const [gridStates, setGridStates] = React.useState(Array(gridSize ** 2).fill('w-4 h-4'));
 
-    console.log(canvasGridComponents.length)
+    const handleMouseDown = (index: any) => {
+        const updatedGridStates = [...gridStates];
+        updatedGridStates[index] = 'w-4 h-4 bg-black';
+        setGridStates(updatedGridStates);
+    };
+
+    const canvasGridComponents = [];
+    for (let i = 0; i < gridSize ** 2; i++) {
+        canvasGridComponents.push(
+            <CanvasGrid
+                key={i}
+                className={gridStates[i]}
+                onMouseDown={() => handleMouseDown(i)}
+                content={<canvas></canvas>}
+            />
+        );
+    }
 
     return (
         <div className="grid-container">
             {canvasGridComponents}
         </div>
-
-    )
+    );
 }
 
 function CanvasGrid(props: any) {
 
     return (
-        <div className="w-4 h-4">
+        <div className={props.className} onMouseDown={props.onMouseDown}>
             {props.content}
         </div>
 
