28839735abfb362110246d94a1af46e1f400a19e
Cadence K <cadencehiggins@gmail.com>
2024-07-03 16:34:43 -0500

    fixing deployment bugs

---
+++ backend/server.py
@@ -0,0 +1,43 @@
+import drawAi
+from flask import request
+from flask import Flask
+from flask_cors import CORS
+import torch
+from waitress import serve
+
+
+app = Flask(__name__, static_url_path='')
+CORS(app)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return app.send_static_file(filename="index.html")
+
+@app.route('/api/aiGuess', methods=['POST', 'GET'])
+def aiGuess():
+    try:
+        userInput = request.json
+        guessed_digit, probabilities = drawAi.guessDigit(model, userInput)
+        if probabilities == "NaN":
+            probabilitiesStats = []
+        else:
+            probabilitiesStats = probabilities.tolist()[0]
+        print(probabilitiesStats)
+        return {
+            "guessed_digit": guessed_digit,
+            "probabilities": probabilitiesStats
+        }
+    except Exception as e:
+        return {
+            "error": str(e)
+        }, 500  # Internal Server Error
+
+
+if __name__ == '__main__':
+    print('begin main')
+    model = drawAi.NeuralNetwork()
+    model.load_state_dict(torch.load("ai/model.pth",  map_location=torch.device('cpu') ))
+    model.eval()
+    print('finsih main')
+    serve(app, host="0.0.0.0", port=5000)
